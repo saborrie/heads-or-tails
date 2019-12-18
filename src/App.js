d30cfe0ebac3b6d8ba34b32df8f42905fb92e806
Steven Borrie <saborrie@gmail.com>
2019-12-18 23:39:24 +0000

    made it work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [article, setArticle] = React.useState(null);
+  const [showLoremIpsum, setShowLoremIpsum] = React.useState("initial");
+
+  async function getRandomArticle() {
+    // const res = await fetch("http://localhost:5000/random");
+    const res = await fetch("https://en.wikipedia.org/api/rest_v1/page/random/title");
+    const json = await res.json();
+    setArticle(json);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <a className="button" onClick={getRandomArticle}>
+        Get random article.
+      </a>
+      {article &&
+        article.items.map(i => {
+          return (
+            <div className="app">
+              <p className="text">{i.title}</p>
+              <a className="button" href={"https://en.wikipedia.org/wiki/" + i.title} target="_blank">
+                HEADS
+              </a>
+              <a className="button" href={"https://en.wikipedia.org/wiki/Lorem_ipsum"} target="_blank">
+                TAILS
+              </a>
+            </div>
+          );
+        })}
     </div>
   );
 }
